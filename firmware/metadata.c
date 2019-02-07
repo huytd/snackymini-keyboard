7f6645a340a0808516914a3e8e439e39e5b59dfd
Huy <kingbazoka@gmail.com>
2019-02-07 13:31:04 -0800

    Use spaces not tabs

--- firmware/metadata.c
+++ firmware/metadata.c
@@ -1,7 +1,7 @@
 #include "usb_names.h"
 
-#define MANUFACTURER_NAME	    {'S','n','a','c','k','y'}
-#define MANUFACTURER_NAME_LEN	6
+#define MANUFACTURER_NAME     {'S','n','a','c','k','y'}
+#define MANUFACTURER_NAME_LEN 6
 #define PRODUCT_NAME          {'S','n','a','c','k','y','M','i','n','i'}
 #define PRODUCT_NAME_LEN      10
 
